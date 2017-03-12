7b7750f3f41c0a8b96e8890d52da74fc99f36d93
Alice Guan <aliguan726@gmail.com>
2017-03-12 00:59:22 -0500

    Create Isometric Maze and added Sprite

--- js/index.js
+++ js/index.js
@@ -1,62 +1,75 @@
+var newTriwizard;
+var createRow;
+var ctx = document.getElementById('isometricMaze').getContext('2d');
+
 function Maze() {
     this.maze = [
-    [1,1,1,1,1,1,1,1],
-    [1,null,null,null,null,null,null,1],
-    [1,null,null,null,null,null,null,1],
-    [1,null,null,null,null,null,null,1],
-    [1,null,null,null,null,null,null,1],
-    [1,null,null,null,null,null,null,1],
-    [1,null,null,null,null,null,null,1],
-    [1,1,1,1,1,1,1,1],
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+    [1,0,0,0,0,0,0,0,0,1,0,1,0,0,0,1,0,0,0,1],
+    [1,0,1,0,1,1,1,1,0,0,0,0,0,1,0,1,0,1,1,1],
+    [1,1,1,0,1,0,0,1,0,1,1,1,1,1,0,1,0,1,0,1],
+    [0,0,0,0,1,0,1,1,0,1,0,0,0,0,0,0,0,0,0,1],
+    [1,1,1,1,1,0,0,0,0,1,0,1,1,1,1,1,1,0,0,1],
+    [1,0,1,0,0,0,1,1,1,1,0,1,0,0,0,0,0,0,0,1],
+    [1,0,1,0,1,1,1,0,1,0,0,1,1,1,1,1,0,0,0,1],
+    [1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,1,0,1,0,1],
+    [1,0,1,0,1,0,0,0,0,1,1,0,1,0,1,1,0,1,0,1],
+    [1,0,0,0,1,1,1,1,0,1,1,0,1,0,0,0,0,1,0,1],
+    [1,1,1,0,0,0,0,1,0,0,0,0,1,0,1,1,1,1,0,1],
+    [1,0,1,1,1,1,0,1,1,0,0,0,1,0,0,0,0,0,0,0],
+    [1,0,0,0,0,1,0,1,1,1,1,1,1,0,1,1,1,0,1,1],
+    [1,0,0,1,1,1,0,1,0,0,0,0,0,0,1,0,0,0,0,1],
+    [1,0,0,0,0,0,0,1,0,1,1,1,1,0,1,0,1,1,0,1],
+    [1,1,0,1,1,1,1,1,0,1,0,0,1,0,1,0,0,1,1,1],
+    [1,0,0,1,0,0,0,0,0,1,0,1,1,0,1,1,0,1,0,1],
+    [1,1,0,0,0,1,0,1,0,1,0,0,0,0,0,0,0,0,0,1],
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
     ];
+    this.tileGraphics = [];
 }
 
 Maze.prototype._renderBoard = function () {
   this.maze.forEach(function(row){ console.log(row); });
 };
 
-var newTriwizard;
-var createRow;
+Maze.prototype._loadTiles =  function() {
+  var tileGraphicsToLoad = ["img/none.png", "img/isohedge.png","img/magic.png"];
 
-Maze.prototype._createMaze = function() {
-    for(var i = 0; i < this.maze.length; i++) {
-        for(var j = i+1; j < this.maze.length; j++) {
-            if(this.maze[i][1] === 1) {
-
-               createRow = document.createElement('div');
-               createRow.className = 'row';
-               $('#maze').append(createRow);
-
-               createCol = document.createElement('div');
-               createCol.className = 'col-sm-7 column';
-               $('.row').append(createCol);
-            }
-
-        }
-     }
-};
-
-Maze.prototype._addHedges = function () {
-    for(var i = 0; i < this.maze.length; i++) {
-        for(var j = i+1; j < this.maze.length; j++) {
-            if(this.maze[i][j] === 1) {
-                $('.column').append('<img src="img/hedge.png"/>');
-            }
-        }
+    for (var i = 0; i < tileGraphicsToLoad.length; i++) {
+      this.tileGraphics[i] = document.createElement("img");
+      this.tileGraphics[i].src = tileGraphicsToLoad[i];
     }
 };
+
+Maze.prototype._drawMap = function() {
+  var harryx = 4;
+  var harryy = 0;
+  var tileH = 25;
+  var tileW = 50;
+
+  var mapX = 700;
+  var mapY = 70;
+  var drawTile;
+
+  for (var i = 0; i < this.maze.length; i++) {
+    for (var j = 0; j < this.maze[i].length; j++) {
+      drawTile = this.maze[i][j];
+      ctx.drawImage(this.tileGraphics[drawTile], (i - j) * tileH + mapX, (i + j) * tileH / 2 + mapY);
+      if(harryx === i && harryy === j) {
+          ctx.drawImage(this.tileGraphics[2], (i - j) * tileH + mapX, (i + j) * tileH / 2 + mapY);
+      }
+    }
+
+  }
+
+};
+
+
 
 
 $( document ).ready(function() {
     newTriwizard = new Maze();
     newTriwizard._renderBoard();
-    for(var i = 0; i < newTriwizard.maze.length; i++) {
-        for(var j = i+1; j < newTriwizard.maze.length; j++) {
-         if(newTriwizard.maze[i][1] === 1) {
-            createRow = document.createElement('div');
-            createRow.className = 'row';
-            $('#maze').append(createRow);
-            }
-        }
-     }
+    newTriwizard._loadTiles();
+    newTriwizard._drawMap();
 });

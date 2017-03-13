89204e7392f817b4089b67b841e74ed9475f8058
Alice Guan <aliguan726@gmail.com>
2017-03-12 20:09:55 -0400

    Populate Harry Potter and trophy

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 var newTriwizard;
-var createRow;
+var tileGraphicsLoaded = 0;
 var ctx = document.getElementById('isometricMaze').getContext('2d');
 
 function Maze() {
@@ -10,14 +10,14 @@ function Maze() {
     [1,1,1,0,1,0,0,1,0,1,1,1,1,1,0,1,0,1,0,1],
     [0,0,0,0,1,0,1,1,0,1,0,0,0,0,0,0,0,0,0,1],
     [1,1,1,1,1,0,0,0,0,1,0,1,1,1,1,1,1,0,0,1],
-    [1,0,1,0,0,0,1,1,1,1,0,1,0,0,0,0,0,0,0,1],
+    [1,0,1,0,0,0,1,0,1,1,0,1,0,0,0,0,0,0,0,1],
     [1,0,1,0,1,1,1,0,1,0,0,1,1,1,1,1,0,0,0,1],
-    [1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,1,0,1,0,1],
-    [1,0,1,0,1,0,0,0,0,1,1,0,1,0,1,1,0,1,0,1],
-    [1,0,0,0,1,1,1,1,0,1,1,0,1,0,0,0,0,1,0,1],
+    [1,0,1,0,1,0,0,0,1,0,0,0,0,0,0,1,0,1,0,1],
+    [1,0,1,0,1,0,0,4,0,0,0,0,1,0,1,1,0,1,0,1],
+    [1,0,0,0,1,1,1,1,0,2,0,1,1,0,0,0,0,1,0,1],
     [1,1,1,0,0,0,0,1,0,0,0,0,1,0,1,1,1,1,0,1],
     [1,0,1,1,1,1,0,1,1,0,0,0,1,0,0,0,0,0,0,0],
-    [1,0,0,0,0,1,0,1,1,1,1,1,1,0,1,1,1,0,1,1],
+    [1,0,0,0,0,1,0,1,1,1,1,1,1,1,1,1,1,0,1,1],
     [1,0,0,1,1,1,0,1,0,0,0,0,0,0,1,0,0,0,0,1],
     [1,0,0,0,0,0,0,1,0,1,1,1,1,0,1,0,1,1,0,1],
     [1,1,0,1,1,1,1,1,0,1,0,0,1,0,1,0,0,1,1,1],
@@ -32,14 +32,20 @@ Maze.prototype._renderBoard = function () {
   this.maze.forEach(function(row){ console.log(row); });
 };
 
+//Preload images
 Maze.prototype._loadTiles =  function() {
-  var tileGraphicsToLoad = ["img/none.png", "img/isohedge.png","img/magic.png"];
+  var tileGraphicsToLoad = ["img/blank.png", "img/isohedge.png","img/trophy.png", "img/harry.png", "img/goblin.png"];
 
     for (var i = 0; i < tileGraphicsToLoad.length; i++) {
       this.tileGraphics[i] = document.createElement("img");
       this.tileGraphics[i].src = tileGraphicsToLoad[i];
     }
 };
+
+// Maze.protoype.move() {
+//     this.maze.addEventListener('keydown')
+// }
+
 
 Maze.prototype._drawMap = function() {
   var harryx = 4;
@@ -50,18 +56,19 @@ Maze.prototype._drawMap = function() {
   var mapX = 700;
   var mapY = 70;
   var drawTile;
-
+  //Loop through 2D Array
   for (var i = 0; i < this.maze.length; i++) {
     for (var j = 0; j < this.maze[i].length; j++) {
       drawTile = this.maze[i][j];
+      //Implement isometric view
       ctx.drawImage(this.tileGraphics[drawTile], (i - j) * tileH + mapX, (i + j) * tileH / 2 + mapY);
+      //Add in Harry Sprite
       if(harryx === i && harryy === j) {
-          ctx.drawImage(this.tileGraphics[2], (i - j) * tileH + mapX, (i + j) * tileH / 2 + mapY);
+          ctx.drawImage(this.tileGraphics[3], (i - j) * tileH + mapX, (i + j) * tileH / 2 + mapY);
       }
     }
 
   }
-
 };
 
 
